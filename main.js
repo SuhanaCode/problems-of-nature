848c7d674410b828de7c704efbd40527b1221a7c
SuhanaCode <87379808+SuhanaCode@users.noreply.github.com>
2022-01-16 19:29:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+function btn1() {
+    window.location = "climate.html";
+}
+
+function btn2() {
+    window.location = "pollution.html";
+}
+
+function btn3() {
+    window.location = "deforestation.html";
+}
+
+function back() {
+    window.location = "index.html";
+}
+
+function redirect() {
+    console.log("Page Loaded");
+    console.log("Redirected to the 'Climate Change' Page");
+}
+
+function redirect1() {
+    console.log("Page Loaded");
+    console.log("Redirected to the 'Pollution' Page");
+}
+
+function redirect2() {
+    console.log("Page Loaded");
+    console.log("Redirected to the 'Deforestation' Page");
+}
